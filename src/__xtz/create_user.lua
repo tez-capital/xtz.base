dda15e698710a4d2ec66244533cab285fe962eb5
V <cryi@tutanota.com>
2023-06-10 22:10:42 +0200

    use user plugin

--- src/__xtz/create_user.lua
+++ src/__xtz/create_user.lua
@@ -1,12 +1,9 @@
 local _user = am.app.get("user", "root")
 ami_assert(type(_user) == "string", "User not specified...", EXIT_INVALID_CONFIGURATION)
 
-local _ok, _uid = fs.safe_getuid(_user)
-if not _ok or not _uid then
-    log_info("Creating user - " .. _user .. "...")
-    local _ok = os.execute('adduser --disabled-login --disabled-password --gecos "" ' .. _user)
-    ami_assert(_ok, "Failed to create user - " .. _user, EXIT_INVALID_CONFIGURATION)
-    log_info("User " .. _user .. " created.")
-else
-    log_info("User " .. _user .. " found.")
-end
+local _ok, _userPlugin = am.plugin.safe_get("user")
+ami_assert(_ok, "Failed to load user plugin - " .. tostring(_userPlugin), EXIT_PLUGIN_LOAD_ERROR)
+
+log_info("Checking user '" .. _user .. "' availability...")
+local _ok = _userPlugin.add(_user, { disableLogin = true, disablePassword = true, gecos = "" })
+ami_assert(_ok, "Failed to create user - " .. _user)
