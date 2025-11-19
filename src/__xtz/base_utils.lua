9032afba1c4c2bab90f8f15548d127b62d1b6e7e
v <cryi@tutanota.com>
2025-11-19 20:10:26 +0100

    support for custom file ownership and permissions

--- src/__xtz/base_utils.lua
+++ src/__xtz/base_utils.lua
@@ -7,9 +7,47 @@ function utils.setup_file_ownership()
 	local uid, err = fs.getuid(user)
 	ami_assert(uid, "failed to get " .. user .. "uid - " .. tostring(err))
 
+    local custom_ownership = am.app.get_model("CUSTOM_FILE_OWNERSHIP", {})
+    ami_assert(type(custom_ownership) == "table", "invalid CUSTOM_FILE_OWNERSHIP configuration")
+
 	log_info("Granting access to " .. user .. "(" .. tostring(uid) .. ")...")
-	local ok, error = fs.chown(os.cwd(), uid, uid, { recurse = true, recurse_ignore_errors = true })
-	ami_assert(ok, "failed to chown data - " .. (error or ""))
+	local ok, error = fs.chown(os.cwd(), uid, uid, { recurse = true, recurse_ignore_errors = true, filter = function(path)
+        if custom_ownership[path] ~= nil then
+            return false
+        end
+        return true
+    end })
+    ami_assert(ok, "failed to chown data - " .. (error or ""))
+
+    for path, ownership in pairs(custom_ownership) do
+        if not fs.exists(path) then
+            goto continue
+        end
+        local custom_uid, err = fs.getuid(ownership.user)
+        ami_assert(custom_uid, "failed to get " .. ownership.user .. " uid - " .. tostring(err))
+        local custom_gid, err = fs.getgid(ownership.group)
+        ami_assert(custom_gid, "failed to get " .. ownership.group .. " gid - " .. tostring(err))
+        log_info("Setting custom ownership for " .. path .. " to " .. ownership.user .. "(" .. tostring(custom_uid) .. "):" .. ownership.group .. "(" .. tostring(custom_gid) .. ")")
+        local ok, error = fs.chown(path, custom_uid, custom_gid, { recurse = true, recurse_ignore_errors = true })
+        ami_assert(ok, "failed to chown " .. path .. " - " .. (error or ""))
+        ::continue::
+    end	
+end
+
+function utils.setup_file_permissions()
+    utils.setup_file_ownership()
+
+    local custom_ownership = am.app.get_model("CUSTOM_FILE_PERMISSIONS", {})
+    ami_assert(type(custom_ownership) == "table", "invalid CUSTOM_FILE_PERMISSIONS configuration")
+    for path, permissions in pairs(custom_ownership) do
+        if not fs.exists(path) then
+            goto continue
+        end
+        log_info("Setting custom permissions for " .. path .. " to " .. permissions)
+        local ok, error = fs.chmod(path, permissions, { recurse = true })
+        ami_assert(ok, "failed to chmod " .. path .. " - " .. (error or ""))
+        ::continue::
+    end
 end
 
 -- Converts URLs to "host:port" format as described:
